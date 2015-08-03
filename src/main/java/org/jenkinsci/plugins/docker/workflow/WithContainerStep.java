a354b4d78d38281fb302bf3a5ff620d4b4b256b0
Jesse Glick <jglick@cloudbees.com>
2015-08-03 16:35:09 -0400

    Noting https://github.com/jenkinsci/workflow-plugin/pull/168.

--- src/main/java/org/jenkinsci/plugins/docker/workflow/WithContainerStep.java
+++ src/main/java/org/jenkinsci/plugins/docker/workflow/WithContainerStep.java
@@ -213,6 +213,7 @@ public class WithContainerStep extends AbstractStepImpl {
 
     }
 
+    // TODO use BodyExecutionCallback.TailCall from https://github.com/jenkinsci/workflow-plugin/pull/168
     private static class Callback extends BodyExecutionCallback {
 
         private static final long serialVersionUID = 1;
