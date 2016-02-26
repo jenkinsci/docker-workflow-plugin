55724929e97d38f2f2b6a0e314cd83ad5d7674f3
Jesse Glick <jglick@cloudbees.com>
2016-02-26 15:57:48 -0500

    Linking to https://github.com/jenkinsci/jenkins/pull/2066.

--- src/main/java/org/jenkinsci/plugins/docker/workflow/WithContainerStep.java
+++ src/main/java/org/jenkinsci/plugins/docker/workflow/WithContainerStep.java
@@ -152,7 +152,7 @@ public class WithContainerStep extends AbstractStepImpl {
             return false;
         }
 
-        // TODO move to WorkspaceList
+        // TODO use https://github.com/jenkinsci/jenkins/pull/2066
         private static FilePath tempDir(FilePath ws) {
             return ws.sibling(ws.getName() + System.getProperty(WorkspaceList.class.getName(), "@") + "tmp");
         }
