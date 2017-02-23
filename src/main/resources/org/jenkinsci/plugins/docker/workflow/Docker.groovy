b5928b7e1fd28d9db55d9bc4e8113edd77a4d70d
Wojciech Duda <wduda@users.noreply.github.com>
2017-02-23 17:17:56 +0100

    added checking for docker client version before executing tag with --force flag

--- src/main/resources/org/jenkinsci/plugins/docker/workflow/Docker.groovy
+++ src/main/resources/org/jenkinsci/plugins/docker/workflow/Docker.groovy
@@ -159,8 +159,15 @@ class Docker implements Serializable {
         public void tag(String tagName = parsedId.tag, boolean force = true) {
             docker.node {
                 def taggedImageName = toQualifiedImageName(parsedId.userAndRepo + ':' + tagName)
-                // TODO as of 1.10.0 --force is deprecated; for 1.12+ do not try it even once
-                docker.script.sh "docker tag --force=${force} ${id} ${taggedImageName} || docker tag ${id} ${taggedImageName}"
+                //get docker version
+                dockerVersion = docker.script.sh(script: "docker version --format '{{.Client.Version}}'", returnStdout: true).trim().split('.')
+                dockerMainVersion = dockerVersion[0].isInteger() ? dockerVersion[0].toInteger() : null
+                dockerMinorVersion = dockerVersion[1].isInteger() ? dockerVersion[1].toInteger() : null
+                if (dockerMainVersion >= 1 && dockerMinorVersion >= 12) {
+                    docker.script.sh "docker tag ${id} ${taggedImageName}"
+                } else {
+                    docker.script.sh "docker tag --force=${force} ${id} ${taggedImageName} || docker tag ${id} ${taggedImageName}"
+                }
                 return taggedImageName;
             }
         }
