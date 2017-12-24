bf8784745c49ee3a3bdd6021d890df8784627491
Ohad David <ohad.david@amdocs.com>
2017-12-24 12:31:45 +0200

    Added ECS cgroup pattern to DockerClient

--- src/test/java/org/jenkinsci/plugins/docker/workflow/client/DockerClientTest.java
+++ src/test/java/org/jenkinsci/plugins/docker/workflow/client/DockerClientTest.java
@@ -97,7 +97,9 @@ public class DockerClientTest {
     		"2:cpu:/docker/3dd988081e7149463c043b5d9c57d7309e079c5e9290f91feba1cc45a04d6a5b",
     		"4:cpuset:/system.slice/docker-3dd988081e7149463c043b5d9c57d7309e079c5e9290f91feba1cc45a04d6a5b.scope",
     		"10:cpu,cpuacct:/docker/a9f3c3932cd81c4a74cc7e0a18c3300255159512f1d000545c42895adaf68932/docker/3dd988081e7149463c043b5d9c57d7309e079c5e9290f91feba1cc45a04d6a5b",
-            "3:cpu:/docker/4193df6bcf5fce75f3fc77f303b2ac06fb664adeb269b959b7ae17b3f8dcf329/3dd988081e7149463c043b5d9c57d7309e079c5e9290f91feba1cc45a04d6a5b"
+            "3:cpu:/docker/4193df6bcf5fce75f3fc77f303b2ac06fb664adeb269b959b7ae17b3f8dcf329/3dd988081e7149463c043b5d9c57d7309e079c5e9290f91feba1cc45a04d6a5b",
+            "7:cpu:/ecs/0410eff2-7e59-4111-823e-1e0d98ef7f30/3dd988081e7149463c043b5d9c57d7309e079c5e9290f91feba1cc45a04d6a5b",
+            "4:cpuset:/ecs/bc586fde-7258-473e-aaa9-9c1606bccad6/3dd988081e7149463c043b5d9c57d7309e079c5e9290f91feba1cc45a04d6a5b",
     	};
     	
     	for (final String possibleCgroupString : possibleCgroupStrings) {
