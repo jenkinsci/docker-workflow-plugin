7aab54228af02e7f08e0f8c9d0b517db1f91bfe2
Marcel Dias <marceldiass@gmail.com>
2017-12-30 21:01:50 -0200

    Remove unused method

--- src/main/java/org/jenkinsci/plugins/docker/workflow/DockerUtils.java
+++ src/main/java/org/jenkinsci/plugins/docker/workflow/DockerUtils.java
@@ -23,11 +23,8 @@
  */
 package org.jenkinsci.plugins.docker.workflow;
 
-import java.util.AbstractMap.SimpleEntry;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.tools.ant.types.Commandline;
 
@@ -62,20 +59,4 @@ public final class DockerUtils {
         }
         return result;
     }
-
-    public static SimpleEntry<String, String> splitArgs(String argString) {
-        //TODO: support complex single/double quotation marks
-        Pattern p = Pattern.compile("^['\"]?(\\w+)=(.*?)['\"]?$");
-
-        Matcher matcher = p.matcher(argString.trim());
-        if (!matcher.matches()) {
-            throw new IllegalArgumentException("Illegal --build-arg parameter syntax: " + argString);
-        }
-
-        String key = matcher.group(1);
-        String value = matcher.group(2);
-
-        return new SimpleEntry<>(key, value);
-    }
-
 }
