5eb813490940ef98326ab55146a3ada27f130239
Nicolas De Loof <nicolas.deloof@gmail.com>
2018-01-23 15:05:50 +0100

    also support for docker-ce
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- src/main/java/org/jenkinsci/plugins/docker/workflow/client/ControlGroup.java
+++ src/main/java/org/jenkinsci/plugins/docker/workflow/client/ControlGroup.java
@@ -53,6 +53,7 @@ public class ControlGroup {
         if (group.contains("/docker/")) {
             // 4:cpuset:/system.slice/docker-3dd988081e7149463c043b5d9c57d7309e079c5e9290f91feba1cc45a04d6a5b.scope
             // 2:cpu:/docker/3dd988081e7149463c043b5d9c57d7309e079c5e9290f91feba1cc45a04d6a5b
+            // 2:cpu:/docker-ce/docker/7cacbc548047c130ae50653548f037285806d49c0c4c1543925cffb8873ed213
             // 10:cpu,cpuacct:/docker/a9f3c3932cd81c4a74cc7e0a18c3300255159512f1d000545c42895adaf68932/docker/3dd988081e7149463c043b5d9c57d7309e079c5e9290f91feba1cc45a04d6a5b
             // 3:cpu:/docker/4193df6bcf5fce75f3fc77f303b2ac06fb664adeb269b959b7ae17b3f8dcf329/3dd988081e7149463c043b5d9c57d7309e079c5e9290f91feba1cc45a04d6a5b
             int i = group.lastIndexOf('/');
