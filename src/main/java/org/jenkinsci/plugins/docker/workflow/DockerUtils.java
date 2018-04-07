2b6cc08616f4a99df5f820ad9083b623f9367f36
Christoph Linder <christoph.linder@dvbern.ch>
2018-04-07 21:01:03 +0200

    further improve parsing of --build-arg (allow all possible arg separator combinations analogous to docker command line)

--- src/main/java/org/jenkinsci/plugins/docker/workflow/DockerUtils.java
+++ src/main/java/org/jenkinsci/plugins/docker/workflow/DockerUtils.java
@@ -34,9 +34,9 @@ import org.apache.tools.ant.types.Commandline;
 public final class DockerUtils {
 
     // marks the parameter format: --build-arg FOO=BAR
-    private static final String BUILD_ARG_SEPARATE = "--build-arg";
+    private static final String BUILD_ARG = "--build-arg";
     // marks the parameter format: --build-arg=FOO=BAR
-    private static final String BUILD_ARG_CONCATENATED = "--build-arg=";
+    private static final String BUILD_ARG_EQUALS = "--build-arg=";
 
     private DockerUtils() {
         // utility class
@@ -54,16 +54,17 @@ public final class DockerUtils {
         for (int i = 0; i < arguments.length; i++) {
             String arg = arguments[i];
             try {
-                if (arg.equals(BUILD_ARG_SEPARATE)) {
+                if (arg.equals(BUILD_ARG)) {
                     String[] buildArgs = Arrays.copyOfRange(arguments, i, arguments.length);
                     SimpleEntry<String, String> parsed = parseBuildArgsArray(buildArgs);
                     result.put(parsed.getKey(), parsed.getValue());
-                } else if (arg.startsWith(BUILD_ARG_CONCATENATED)) {
+                } else if (arg.startsWith(BUILD_ARG_EQUALS)) {
                     SimpleEntry<String, String> parsed = parseBuildArgsConcatenated(arg);
                     result.put(parsed.getKey(), parsed.getValue());
                 }
             } catch (ParseException pe) {
-                throw new IllegalArgumentException("Error parsing --build-arg: " + pe.getMessage() + ", cmdline: " + commandLineString);
+                throw new IllegalArgumentException("Error parsing --build-arg: " + pe.getMessage() + ", cmdline: " +
+                    commandLineString);
             }
         }
         return result;
@@ -73,19 +74,19 @@ public final class DockerUtils {
      * Requires input: --build-arg=FOO=BAR
      */
     private static SimpleEntry<String, String> parseBuildArgsConcatenated(String concatenated) throws ParseException {
-        String args = concatenated.substring(BUILD_ARG_CONCATENATED.length());
+        String args = concatenated.substring(BUILD_ARG_EQUALS.length());
 
         SimpleEntry<String, String> result = splitArgNameValue(args);
         return result;
     }
 
     /**
-     * Requires input of the format: ["--build-args", "FOO=BAR", ...]
+     * Requires input of the format: ["--build-arg", "FOO=BAR", ...]
      */
     private static SimpleEntry<String, String> parseBuildArgsArray(String arguments[]) throws ParseException {
         if (arguments.length < 2) {
-			throw new ParseException("Missing parameter for --build-arg", 0);
-		}
+            throw new ParseException("Missing parameter for --build-arg", 0);
+        }
         String keyVal = arguments[1];
 
         SimpleEntry<String, String> result = splitArgNameValue(keyVal);
@@ -94,13 +95,15 @@ public final class DockerUtils {
 
     private static SimpleEntry<String, String> splitArgNameValue(String nameValue) throws ParseException {
         String parts[] = nameValue.split("=", 2);
-        if (parts.length != 2) {
-			throw new ParseException("Illegal syntax for --build-arg " + nameValue +
-                ": need further arguments with KEY=VALUE, e.g.: --build-arg FOO=Bar or --build-arg=FOO=Bar", 0);
-		}
-        String key = parts[0];
-        String value = parts[1];
-
-        return new SimpleEntry<>(key, value);
+        if (parts.length == 1) {
+            // ARG with no value => docker uses default
+            return new SimpleEntry<>(parts[0], "");
+        }
+        if (parts.length == 2) {
+            // ARG=VALUE
+            return new SimpleEntry<>(parts[0], parts[1]);
+        }
+        throw new ParseException("Illegal syntax for --build-arg " + nameValue +
+            ": need further arguments with KEY=VALUE, e.g.: --build-arg FOO=Bar or --build-arg=FOO=Bar", 0);
     }
 }
--- src/test/java/org/jenkinsci/plugins/docker/workflow/DockerUtilsTest.java
+++ src/test/java/org/jenkinsci/plugins/docker/workflow/DockerUtilsTest.java
@@ -23,99 +23,184 @@
  */
 package org.jenkinsci.plugins.docker.workflow;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import hudson.FilePath;
-import org.hamcrest.collection.IsCollectionWithSize;
-import org.hamcrest.core.IsCollectionContaining;
-import org.hamcrest.core.IsEqual;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
+import static com.google.common.collect.ImmutableMultimap.of;
+import static java.util.Collections.emptyList;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.isIn;
+import static org.junit.Assert.assertThat;
 
+@RunWith(Parameterized.class)
 public class DockerUtilsTest {
 
-    @Rule public final ExpectedException exception = ExpectedException.none();
-
-    @Test public void parseBuildArgs() throws IOException, InterruptedException {
-
-        FilePath dockerfilePath = new FilePath(new File("src/test/resources/Dockerfile-withArgs"));
-        Dockerfile dockerfile = new Dockerfile(dockerfilePath);
-
-        final String imageToUpdate = "hello-world:latest";
-        final String key = "IMAGE_TO_UPDATE";
-        final String commandLine = "docker build -t hello-world --build-arg "+key+"="+imageToUpdate;
-        Map<String, String> buildArgs = DockerUtils.parseBuildArgs(dockerfile, commandLine);
-
-        Assert.assertThat(buildArgs.keySet(), IsCollectionWithSize.hasSize(1));
-        Assert.assertThat(buildArgs.keySet(), IsCollectionContaining.hasItems(key));
-        Assert.assertThat(buildArgs.get(key), IsEqual.equalTo(imageToUpdate));
+    // parameters trigger the possible valid(!) combinations of:
+    //
+    // Param0: should --build-arg and Param-Name be separated by '=' or by ' '?
+    // examples:
+    // --build-arg FOO=BAR
+    // --build-arg=FOO=BAR
+    //
+    // Param1: should an empty parameter (i.e.: key without value) have the '=' suffix?
+    // examples
+    // --build-arg FOO
+    // --build-arg FOO=
+    @Parameters(name = "{0}/{1}")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            { '=', true },
+            { '=', false },
+            { ' ', true },
+            { ' ', false },
+        });
     }
 
-    @Test public void parseBuildArgsWithDefaults() throws IOException, InterruptedException {
+    @Parameter()
+    public char buildArgsSeparator;
 
-        Dockerfile dockerfile = getDockerfileDefaultArgs();
+    @Parameter(1)
+    public boolean argValuePrefix;
 
-        final String registry = "";
-        final String key_registry = "REGISTRY_URL";
-        final String key_tag = "TAG";
-        final String commandLine = "docker build -t hello-world";
-        Map<String, String> buildArgs = DockerUtils.parseBuildArgs(dockerfile, commandLine);
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
-        Assert.assertThat(buildArgs.keySet(), IsCollectionWithSize.hasSize(2));
-        Assert.assertThat(buildArgs.keySet(), IsCollectionContaining.hasItems(key_registry, key_tag));
-        Assert.assertThat(buildArgs.get(key_registry), IsEqual.equalTo(registry));
-        Assert.assertThat(buildArgs.get(key_tag), IsEqual.equalTo("latest"));
+    // given
+    private Dockerfile dockerfile = null;
+    private Multimap<String, String> buildArgs = null;
+    private String commandLine = null;
+
+    // when
+    private Map<String, String> parsedArgs = null;
+
+    @Test
+    public void parseBuildArgs() {
+        givenDockerfile("src/test/resources/Dockerfile-withArgs");
+        givenBuildArgs(of("IMAGE_TO_UPDATE", "hello-world:latest"));
+        givenCommandLine(withBuildArgsBySeparator());
+
+        whenCommandLineIsParsed();
+
+        thenParsedArgsContainExactly(buildArgs);
     }
 
-    @Test public void parseBuildArgsOverridingDefaults() throws IOException, InterruptedException {
+    @Test
+    public void parseBuildArgsWithDefaults() {
+        givenDockerfileDefaultArgs();
+        givenBuildArgs(of());
+        givenCommandLine(emptyList());
 
-        Dockerfile dockerfile = getDockerfileDefaultArgs();
+        whenCommandLineIsParsed();
 
-        final String registry = "http://private.registry:5000/";
-        final String key_registry = "REGISTRY_URL";
-        final String key_tag = "TAG";
-        final String tag = "1.2.3";
-        final String commandLine = "docker build -t hello-world --build-arg "+key_tag+"="+tag+
-            " --build-arg "+key_registry+"="+registry;
-        Map<String, String> buildArgs = DockerUtils.parseBuildArgs(dockerfile, commandLine);
-
-        Assert.assertThat(buildArgs.keySet(), IsCollectionWithSize.hasSize(2));
-        Assert.assertThat(buildArgs.keySet(), IsCollectionContaining.hasItems(key_registry, key_tag));
-        Assert.assertThat(buildArgs.get(key_registry), IsEqual.equalTo(registry));
-        Assert.assertThat(buildArgs.get(key_tag), IsEqual.equalTo(tag));
+        // these are the defaults defined in the Dockerfile
+        thenParsedArgsContainExactly
+            (of("REGISTRY_URL", "",
+                "TAG", "latest"));
     }
 
-    @Test public void parseBuildArgWithKeyAndEqual() throws IOException, InterruptedException {
-        final String commandLine = "docker build -t hello-world --build-arg key=";
+    @Test
+    public void parseBuildArgsOverridingDefaults() {
+        givenDockerfileDefaultArgs();
+        givenBuildArgs(
+            of("TAG", "1.2.3",
+                "REGISTRY_URL", "http://private.registry:5000/"));
+        givenCommandLine(withBuildArgsBySeparator());
 
-        Map<String, String> buildArgs = DockerUtils.parseBuildArgs(null, commandLine);
+        whenCommandLineIsParsed();
 
-        Assert.assertThat(buildArgs.keySet(), IsCollectionWithSize.hasSize(1));
-        Assert.assertThat(buildArgs.keySet(), IsCollectionContaining.hasItems("key"));
-        Assert.assertThat(buildArgs.get("key"), IsEqual.equalTo(""));
+        thenParsedArgsContainExactly(buildArgs);
     }
 
-    @Test public void parseInvalidBuildArg() throws IOException, InterruptedException {
+    @Test
+    public void parseBuildArgWithKeyAndEqual() {
+        givenNoDockerfile();
+        givenBuildArgs(of("key", ""));
+        givenCommandLine(withBuildArgsBySeparator());
+
+        whenCommandLineIsParsed();
+
+        thenParsedArgsContainExactly(of("key", ""));
+    }
+
+    @Test
+    public void parseInvalidBuildArg() {
         final String commandLine = "docker build -t hello-world --build-arg";
 
         exception.expect(IllegalArgumentException.class);
         DockerUtils.parseBuildArgs(null, commandLine);
     }
 
-    @Test public void parseInvalidBuildArgWithKeyOnly() throws IOException, InterruptedException {
-        final String commandLine = "docker build -t hello-world --build-arg key";
-
-        exception.expect(IllegalArgumentException.class);
-        DockerUtils.parseBuildArgs(null, commandLine);
+    private void givenNoDockerfile() {
+        dockerfile = null;
     }
 
-    private Dockerfile getDockerfileDefaultArgs() throws IOException, InterruptedException {
-        FilePath dockerfilePath = new FilePath(new File("src/test/resources/Dockerfile-defaultArgs"));
-        return new Dockerfile(dockerfilePath);
+    private void givenDockerfile(String dockerFilePath) {
+        FilePath dockerfilePath = new FilePath(new File(dockerFilePath));
+        try {
+            dockerfile = new Dockerfile(dockerfilePath);
+        } catch (Exception e) {
+            throw new IllegalStateException("Dockerfile not found: " + dockerFilePath);
+        }
+    }
+
+    private void givenDockerfileDefaultArgs() {
+        givenDockerfile("src/test/resources/Dockerfile-defaultArgs");
+    }
+
+    private void givenBuildArgs(Multimap<String, String> args) {
+        buildArgs = ImmutableMultimap.copyOf(args);
+    }
+
+    private List<String> withBuildArgsBySeparator() {
+        return buildArgs.entries().stream()
+            .map(param -> createBuildArgParam(buildArgsSeparator, argValuePrefix, param))
+            .collect(Collectors.toList());
+    }
+
+    private void givenCommandLine(List<String> buildArgs) {
+        StringBuilder cmd = new StringBuilder("docker build -t hello-world");
+        for (String arg : buildArgs) {
+            cmd.append(" ").append(arg);
+        }
+        this.commandLine = cmd.toString();
+    }
+
+    private void whenCommandLineIsParsed() {
+        parsedArgs = DockerUtils.parseBuildArgs(dockerfile, commandLine);
+    }
+
+    private void thenParsedArgsContainExactly(Multimap<String, String> expected) {
+        assertThat(parsedArgs.entrySet(), everyItem(isIn(expected.entries())));
+        assertThat(expected.entries(), everyItem(isIn(parsedArgs.entrySet())));
+    }
+
+    private String createBuildArgParam(char buildArgSeparator, boolean emptyValuePrefixEquals,
+        Entry<String, String> param) {
+        String result = " --build-arg" + buildArgSeparator + joinValue(emptyValuePrefixEquals, param);
+        return result;
+    }
+
+    private String joinValue(boolean emptyValuePrefixEquals, Entry<String, String> param) {
+        if (isNotBlank(param.getValue())) {
+            return param.getKey() + "=" + param.getValue();
+        }
+        return param.getKey() + (emptyValuePrefixEquals ? "=" : "");
     }
 }
 
