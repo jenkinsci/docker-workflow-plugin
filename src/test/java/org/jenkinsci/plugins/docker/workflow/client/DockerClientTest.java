08c0a87b3e6ad0b32df2e8af0a49d4f4848c4341
Alex Pykavy <apykavy@aras.com>
2021-02-27 13:58:08 +0300

    Add tests for docker run with environment variables
    To check that they are correctly escaped.

--- src/test/java/org/jenkinsci/plugins/docker/workflow/client/DockerClientTest.java
+++ src/test/java/org/jenkinsci/plugins/docker/workflow/client/DockerClientTest.java
@@ -36,6 +36,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
@@ -68,6 +69,41 @@ public class DockerClientTest {
         Assert.assertTrue(containerRecord.getHost().length() > 0);
         Assert.assertTrue(containerRecord.getCreated() > 1000000000000L);
         Assert.assertEquals(Collections.<String>emptyList(), dockerClient.getVolumes(launchEnv, containerId));
+
+        // Also test that the stop works and cleans up after itself
+        Assert.assertNotNull(dockerClient.inspect(launchEnv, containerId, ".Name"));
+        dockerClient.stop(launchEnv, containerId);
+        Assert.assertNull(dockerClient.inspect(launchEnv, containerId, ".Name"));
+    }
+
+    @Test
+    public void test_run_with_env_vars() throws IOException, InterruptedException {
+        EnvVars launchEnv = DockerTestUtil.newDockerLaunchEnv();
+        EnvVars containerEnv = new EnvVars(
+            "VAR", "A",
+            "VAR_WITH_SPACE", "A B",
+            "VAR_WITH_EQUALS", "A=B",
+            "VAR_WITH_QUOTE", "A\"B",
+            "VAR_WITH_SPACE_AND_EQUALS", "A=B C",
+            "VAR_WITH_SPACE_EQUALS_AND_QUOTE", "A=\"B C\""
+        );
+        String containerId =
+                dockerClient.run(launchEnv, "learn/tutorial", null, null, Collections.<String, String>emptyMap(), Collections.<String>emptyList(), containerEnv,
+                        dockerClient.whoAmI(), "cat");
+        Assert.assertEquals(64, containerId.length());
+        ContainerRecord containerRecord = dockerClient.getContainerRecord(launchEnv, containerId);
+        Assert.assertEquals(dockerClient.inspect(launchEnv, "learn/tutorial", ".Id"), containerRecord.getImageId());
+        Assert.assertTrue(containerRecord.getContainerName().length() > 0);
+        Assert.assertTrue(containerRecord.getHost().length() > 0);
+        Assert.assertTrue(containerRecord.getCreated() > 1000000000000L);
+        Assert.assertEquals(Collections.<String>emptyList(), dockerClient.getVolumes(launchEnv, containerId));
+
+        String envVarsString = dockerClient.inspect(launchEnv, containerId, ".Config.Env");
+        Assert.assertNotNull(envVarsString);
+
+        for (Map.Entry<String, String> variable : containerEnv.entrySet()) {
+            Assert.assertTrue(envVarsString.contains(variable.getKey() + "=" + variable.getValue()));
+        }
 
         // Also test that the stop works and cleans up after itself
         Assert.assertNotNull(dockerClient.inspect(launchEnv, containerId, ".Name"));
